97eb2da63655a83c3384aed09156678b01738286
Mike Kiernan <mkiernan@microsoft.com>
2017-09-19 15:06:36 +0100

    fix script

--- installtools.sh
+++ installtools.sh
@@ -18,6 +18,7 @@ sudo apt-get update
 sudo apt-get install -y build-essential g++ git gcc make cmake htop autotools-dev libicu-dev libbz2-dev libboost-all-dev libssl-dev libffi-dev libpython-dev python-dev python-pip pip python3-pip zip
 pip install --upgrade pip
 apt-get install -y redis-tools
+echo "##############################################################################"
 
 # Install azure-cli
 # https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
@@ -27,6 +28,7 @@ apt-get install -y apt-transport-https
 apt-get update && sudo apt-get install -y azure-cli
 # Configure azure cli
 # https://docs.microsoft.com/en-us/cli/azure/format-output-azure-cli?view=azure-cli-latest
+mkdir /home/$ADMIN/.azure
 cat <<'EOF' >> /home/$ADMIN/.azure/config
 [cloud]
 name = AzureCloud
@@ -39,10 +41,12 @@ collect_telemetry = yes
 [logging]
 enable_log_file = yes
 EOF
-
+echo "# azurecli ###################################################################"
+#
 # Install azure batch cli extensions
 # https://github.com/Azure/azure-batch-cli-extensions
 /opt/az/bin/python3 -m pip install azure-cli-batch-extensions
+echo "#cli extensions###############################################################"
 
 # Install DOTNET & azcopy
 # https://www.microsoft.com/net/core#linuxubuntu
@@ -51,19 +55,23 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-zesty-prod zesty main" > /etc/apt/sources.list.d/dotnetdev.list
 apt-get install -y dotnet-sdk-2.0.0
+echo "# dotnet ######################################################################"
 #echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-zesty-prod zesty main" > /etc/apt/sources.list.d/microsoft-prod.list
 #sudo apt-get install azcopy
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
 tar -xvf azcopy.tar.gz
 ./install.sh
+echo "# azcopy #####################################################################"
 
 # Install Docker
 # https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-repository
 apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sleep 5
 apt-get install -y docker-ce
+echo "# docker #####################################################################"
 
 # Install Batch Shipyard - Ensure you are NOT root for this section
 # https://github.com/Azure/batch-shipyard/blob/master/docs/01-batch-shipyard-installation.md
@@ -73,4 +81,5 @@ tar -xvf $SYVERSION.tar.gz; \
 cd batch-shipyard-$SYVERSION;\
 ./install.sh -3;\
 echo "export PATH=$PATH:$HOME/.local/bin:$SHIPYARD" >> ~/.bashrc
+echo "# shipyard ###################################################################"
 '
